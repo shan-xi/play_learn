c82ee7a8a21b21daf5eaa492e801e00017e9535f
spinliaomarais <spinliao@storemarais.com>
2023-04-14 09:51:50 +0800

    A first iteration of the data model

--- yabe/test/BasicTest.java
+++ yabe/test/BasicTest.java
@@ -124,4 +124,33 @@ public class BasicTest extends UnitTest {
         assertEquals(0, Post.count());
         assertEquals(0, Comment.count());
     }
+
+    @Test
+    public void fullTest(){
+        Fixtures.loadModels("data.yml");
+        assertEquals(2, User.count());
+        assertEquals(3, Post.count());
+        assertEquals(3, Comment.count());
+
+        assertNotNull(User.connect("bob@gmail.com", "secret"));
+        assertNotNull(User.connect("jeff@gmail.com", "secret"));
+        assertNull(User.connect("jeff@gmail.com", "badpassword"));
+        assertNull(User.connect("tom@gmail.com", "secret"));
+
+        List<Post> bobPosts = Post.find("author.email", "bob@gmail.com").fetch();
+        assertEquals(2, bobPosts.size());
+
+        List<Comment> bobComments = Comment.find("post.author.email", "bob@gmail.com").fetch();
+        assertEquals(3, bobComments.size());
+
+        Post frontPost = Post.find("order by postedAt desc").first();
+        assertNotNull(frontPost);
+        assertEquals("About the model layer", frontPost.title);
+
+        assertEquals(2, frontPost.comments.size());
+
+        frontPost.addComment("Jim", "Hello guys");
+        assertEquals(3, frontPost.comments.size());
+        assertEquals(4, Comment.count());
+    }
 }
