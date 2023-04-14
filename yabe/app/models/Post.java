08236c2d7adb7f05a6cfc79b28fda21590516daf
spinliaomarais <spinliao@storemarais.com>
2023-04-14 12:43:03 +0800

    Adding tagging support

--- yabe/app/models/Post.java
+++ yabe/app/models/Post.java
@@ -3,9 +3,7 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Post extends Model {
@@ -22,8 +20,12 @@ public class Post extends Model {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     public List<Comment> comments;
 
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    public Set<Tag> tags;
+
     public Post(User author, String title, String content) {
         this.comments = new ArrayList<Comment>();
+        this.tags = new TreeSet<Tag>();
         this.author = author;
         this.title = title;
         this.content = content;
@@ -43,5 +45,21 @@ public class Post extends Model {
 
     public Post next() {
         return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+    }
+
+    public Post tagItWith(String name) {
+        tags.add(Tag.findOrCreateByName(name));
+        return this;
+    }
+
+    public static List<Post> findTaggedWith(String tag) {
+        return Post.find(
+                "select distinct p from Post p join p.tags as t where t.name = ?", tag
+        ).fetch();
+    }
+    public static List<Post> findTaggedWith(String... tags) {
+        return Post.find(
+                "select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content,p.postedAt having count(t.id) = :size"
+        ).bind("tags", tags).bind("size", tags.length).fetch();
     }
 }
